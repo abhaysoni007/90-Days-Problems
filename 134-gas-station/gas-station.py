8c14239cc0e60d55bc0d1a60daf4882b27886c62
Abhay <yuvrajsoni411@gmail.com>
2026-04-01 15:00:16 +0530

    Time: 43 ms (14.90%) | Memory: 26 MB (50.17%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -4,14 +4,14 @@ class Solution:
         start = 0
         total_gas = 0
         total_cost = 0
-        
         for i in range(len(gas)):
             total_gas += gas[i]
             total_cost += cost[i]
             tank += gas[i] - cost[i]
-            
             if tank < 0:
                 start = i + 1
                 tank = 0
-        
-        return start if total_gas >= total_cost else -1
+        if total_gas >= total_cost:
+            return start
+        else:
+            return -1
